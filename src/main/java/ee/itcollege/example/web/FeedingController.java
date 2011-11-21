9f73cd27a15371c597e0c181bd4bb624d5db9e9c
Antti Andreimann <Antti.Andreimann@mail.ee>
2011-11-21 14:34:18 +0200

    Loeng 11: Add security and i18n

--- src/main/java/ee/itcollege/example/web/FeedingController.java
+++ src/main/java/ee/itcollege/example/web/FeedingController.java
@@ -12,13 +12,19 @@ import ee.itcollege.example.entities.Cage;
 @RequestMapping("/feeding/**")
 @Controller
 public class FeedingController {
+    private transient org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(getClass());
+
     @RequestMapping
     public String index(Model uiModel) {
+        log.debug("index(...)");
+
     	List<FeedingRation> data = new ArrayList<FeedingRation>();
     	
     	for(Cage c: Cage.findAllCages()) {
+            log.debug("Processing cage: {} ({})", new Object[] { c.getName(), c.getId() });
     		data.add(FeedingRation.findRationsForCage(c));
     	}
+        log.debug("Number of Feeding rations loaded: {}", data.size());
     	
     	uiModel.addAttribute("data", data);
         return "feeding/index";
